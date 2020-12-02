e43eb8aeffa2e587d214d2475c16662dc822062c
Claire <claire@Claires-MacBook-Pro.local>
2020-12-02 21:47:56 +0000

    Set up files

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Random Number Generator
+// Function that checks random number against Input
+// Function - too high
+// function too low
+// function correct
+// restart
+// set high score
